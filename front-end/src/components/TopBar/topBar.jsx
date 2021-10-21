fdc29e745d42aa803ab01cc511c2479f143bc3d3
Des-cloud <61790033+Des-cloud@users.noreply.github.com>
2021-10-21 01:13:16 -0400

    Remove placeholder menu items

--- front-end/src/components/TopBar/topBar.jsx
+++ front-end/src/components/TopBar/topBar.jsx
@@ -78,27 +78,7 @@ export default function TopBar() {
                     </IconButton>
                 </DrawerHeader>
                 <Divider />
-                <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                    <ListItem button key={text}>
-                    <ListItemIcon>
-                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                    </ListItemIcon>
-                    <ListItemText primary={text} />
-                    </ListItem>
-                ))}
-                </List>
                 <Divider />
-                <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                    <ListItem button key={text}>
-                    <ListItemIcon>
-                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                    </ListItemIcon>
-                    <ListItemText primary={text} />
-                    </ListItem>
-                ))}
-                </List>
             </Drawer>
         </Toolbar>
         </AppBar>
