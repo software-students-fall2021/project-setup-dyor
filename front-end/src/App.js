870f81602087e42ee3d127efa8160a71206a7dfe
Abdullah Javaid Chaudhry <aqhr050@gmail.com>
2021-10-23 14:26:15 -0400

    freshInstall(Node-v14;NPM-v8)

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import TopBar from './components/TopBar/topBar';
 
 function App() {
   return (
-    <TopBar />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
