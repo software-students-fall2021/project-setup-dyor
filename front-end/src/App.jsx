59a64e35b9f8b3ca9e6dc25bf7ca9ad8f068486b
Des-cloud <61790033+Des-cloud@users.noreply.github.com>
2021-10-23 12:49:07 -0400

    Modified top bar and app.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css';
 import TopBar from './components/TopBar/topBar';
 import NewsTile from './components/NewsTile/newsTile'
+import Footer from './components/Footer/footer';
 
 function App() {
   return (
     <div className="App">
       <TopBar />
       <NewsTile />
+      <NewsTile />
+      <NewsTile />
+      <NewsTile />
+      <NewsTile />
+      <NewsTile />
+      <NewsTile />
+      <Footer />
     </div>
   );
 }
