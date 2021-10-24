82347c519eae40d425f3db85366be942e74ad19b
Hanzalla Usman <hu297@nyu.edu>
2021-10-24 17:07:04 -0400

    fixed relative address

--- front-end/src/containers/PortfolioPage/PortfolioPage.jsx
+++ front-end/src/containers/PortfolioPage/PortfolioPage.jsx
@@ -1,8 +1,8 @@
 import { PortfolioTable } from "../../components/PortfolioTable/PortfolioTable";
 import Typography from "@mui/material/Typography";
-import styles from "/Users/hanzallausman/Documents/Software Engr/project-setup-dyor/front-end/src/containers/PortfolioPage/PortfolioPage.module.css";
+import styles from "./PortfolioPage.module.css";
 import { Box } from "@mui/system";
-import { DailyGraph } from "/Users/hanzallausman/Documents/Software Engr/project-setup-dyor/front-end/src/components/DailyGraph/DailyGraph.jsx";
+import { DailyGraph } from "../../components/DailyGraph/DailyGraph.jsx";
 
 export function PortfolioPage() {
   return (
