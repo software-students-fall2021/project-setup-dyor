c3d8274767bfc3b9002b50df6a487b63352a093b
Abdullah Javaid Chaudhry <aqhr050@gmail.com>
2021-10-25 17:35:16 -0400

    Difficulty when merging earlier, will push into Master directly, now

--- front-end/src/components/Forms/AddAssetForm.jsx
+++ front-end/src/components/Forms/AddAssetForm.jsx
@@ -1,8 +1,28 @@
 import React, { useState, useEffect } from "react";
+import { Grid } from "@material-ui/core";
 import styles from "./AddAssetForm.module.css";
 
 const AddAssetForm = (props) => {
-    return <h2>Add Asset Form</h2>;
+    const [newAssetValues, setnewAssetValues] = useState({});
+
+    return (
+        <form>
+            <Grid container className={styles.addAssetFormContainer}>
+                <Grid item xs={6} md={3}>
+                    Coin
+                </Grid>
+                <Grid item xs={6} md={3}>
+                    Quantity Purchased
+                </Grid>
+                <Grid item xs={6} md={3}>
+                    Unit Price
+                </Grid>
+                <Grid item xs={6} md={3}>
+                    Salam Four
+                </Grid>
+            </Grid>
+        </form>
+    );
 };
 
 export default AddAssetForm;
