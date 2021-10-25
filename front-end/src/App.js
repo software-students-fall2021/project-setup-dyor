b1910f6544385f2877fb0921d4197ccb7069a41b
Hanzalla Usman <hu297@nyu.edu>
2021-10-25 16:42:24 -0400

    Changed style of portfolio page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,22 +4,24 @@ import * as React from "react";
 
 import "./App.css";
 import { BottomBar } from "./components/BottomBar/BottomBar";
+import TopBar from "./components/TopBar/TopBar";
 // import Button from "@mui/material/Button";
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: "#088F8F",
-        },
-        secondary: {
-            main: "#0D98BA",
-        },
+  palette: {
+    primary: {
+      main: "#088F8F",
     },
+    secondary: {
+      main: "#0D98BA",
+    },
+  },
 });
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <TopBar />
       <PortfolioPage></PortfolioPage>
       <BottomBar></BottomBar>
     </ThemeProvider>
