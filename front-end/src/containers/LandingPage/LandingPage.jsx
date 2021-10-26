0b75c23a30064239730828bd9cd55303dceee5a6
Hanzalla Usman <hu297@nyu.edu>
2021-10-26 05:58:02 -0400

    dependency reinstallation

--- front-end/src/containers/LandingPage/LandingPage.jsx
+++ front-end/src/containers/LandingPage/LandingPage.jsx
@@ -1,30 +1,28 @@
 import React from "react";
-import {makeStyles} from '@material-ui/core/styles'
+import { makeStyles } from "@mui/styles";
 import Header from "../../components/HeaderforLandingPage/Header";
 import { PortfolioCard } from "../../components/FeaturesforLandingPage/PortfolioCard";
 import { CssBaseline } from "@material-ui/core";
 
-
 const useStyles = makeStyles((theme) => ({
-    root:{
-      minHeight: '100vh',
-      backgroundImage: `url(${process.env.PUBLIC_URL + "/Back_2.png"})`,
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-  
-    },
-  }));
+  root: {
+    minHeight: "100vh",
+    backgroundImage: `url(${process.env.PUBLIC_URL + "/Back_2.png"})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+  },
+}));
 
 const LandingPage = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-    <div className = {classes.root}>
-      <CssBaseline/>
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
       <Header />
-      <PortfolioCard/>
-    </div>);
-}
+      <PortfolioCard />
+    </div>
+  );
+};
 
 export default LandingPage;
-    
