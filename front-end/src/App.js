de2307418767eb072f0b134df55437149b4f4088
Umair Shah <umair.shah@live.com>
2021-10-26 00:17:57 -0400

    Made landing page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,13 @@
 import { PortfolioPage } from "./containers/PortfolioPage/PortfolioPage";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import * as React from "react";
+import {makeStyles} from '@material-ui/core/styles'
+import Header from "./components/HeaderforLandingPage/Header";
+import { Portfolio } from "./components/FeaturesforLandingPage/Portfolio";
 
 import "./App.css";
 import { BottomBar } from "./components/BottomBar/BottomBar";
+import { CssBaseline } from "@material-ui/core";
 // import Button from "@mui/material/Button";
 
 const theme = createTheme({
@@ -17,12 +21,28 @@ const theme = createTheme({
     },
 });
 
+const useStyles = makeStyles((theme) => ({
+  root:{
+    minHeight: '100vh',
+    backgroundImage: `url(${process.env.PUBLIC_URL + "/Back_2.png"})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+
+  },
+}));
+
 function App() {
+  const classes = useStyles();
   return (
-    <ThemeProvider theme={theme}>
-      <PortfolioPage></PortfolioPage>
-      <BottomBar></BottomBar>
-    </ThemeProvider>
+    // <ThemeProvider theme={theme}>
+    //   <PortfolioPage></PortfolioPage>
+    //   <BottomBar></BottomBar>
+    // </ThemeProvider>
+    <div className = {classes.root}>
+    <CssBaseline/>
+    <Header />
+    <Portfolio/>
+  </div>
   );
 }
 
