09f7a954c8c5b9ed9b9f40761664bf13424a60e4
sashanksilwal <sashanksilwal@hotmail.com>
2021-10-26 23:49:36 -0400

    adding styles to sign in and sign up pages

--- front-end/src/containers/LoginPage/LoginPage.jsx
+++ front-end/src/containers/LoginPage/LoginPage.jsx
@@ -1,59 +1,69 @@
 import React, { useState } from "react";
 import { Button, Stack, TextField, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
+import { Paper } from "@mui/material";
+import style from "./LoginPage.module.css";
 
 const LoginPage = ({ loginHandler }) => {
-    const [userInput, setUserInput] = useState({ email: "", password: "" });
+  const [userInput, setUserInput] = useState({ email: "", password: "" });
 
-    const handleInputChange = (event) => {
-        if (event) {
-            const { id, value } = event.target;
-            setUserInput((prevUserInput) => ({
-                ...prevUserInput,
-                [id]: value,
-            }));
-        }
-    };
+  const handleInputChange = (event) => {
+    if (event) {
+      const { id, value } = event.target;
+      setUserInput((prevUserInput) => ({
+        ...prevUserInput,
+        [id]: value,
+      }));
+    }
+  };
 
-    return (
-        <Stack
-            direction="column"
-            justifyContent="space-evenly"
-            alignItems="center"
-            spacing={2}
-        >
-            <item>
-                <Typography>Welcome back!</Typography>
-            </item>
-            <item>
-                <TextField
-                    fullWidth
-                    variant="outlined"
-                    id="email"
-                    label="Email Address"
-                    value={userInput.email}
-                    onChange={handleInputChange}
-                ></TextField>
-            </item>
-            <item>
-                <TextField
-                    fullWidth
-                    variant="outlined"
-                    id="password"
-                    label="Password"
-                    value={userInput.password}
-                    onChange={handleInputChange}
-                ></TextField>
-            </item>
-            <item>
-                <Link to="/dashboard">
-                    <Button color="primary" onClick={loginHandler}>
-                        Login
-                    </Button>
-                </Link>
-            </item>
-        </Stack>
-    );
+  return (
+    <div className={style.bgColor}>
+      <Stack
+        direction="column"
+        justifyContent="space-evenly"
+        alignItems="center"
+        spacing={2}
+      >
+        <Paper elevation={2} className={style.cardBox}>
+          <div className={style.centerButton}>
+            <Typography className={style.greetings}>Welcome back!</Typography>
+          </div>
+          <item>
+            <TextField
+              fullWidth
+              variant="outlined"
+              id="email"
+              label="Email Address"
+              value={userInput.email}
+              onChange={handleInputChange}
+            ></TextField>
+          </item>
+          <item>
+            <TextField
+              fullWidth
+              variant="outlined"
+              id="password"
+              label="Password"
+              value={userInput.password}
+              onChange={handleInputChange}
+            ></TextField>
+          </item>
+          <div className={style.centerButton}>
+            <Link to="/dashboard">
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={loginHandler}
+              >
+                Login
+              </Button>
+            </Link>
+          </div>
+        </Paper>
+      </Stack>
+    </div>
+  );
 };
 
 export default LoginPage;
