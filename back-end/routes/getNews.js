facb17c63e592af83d0791d887dfddc26f1c86c1
Des-cloud <61790033+Des-cloud@users.noreply.github.com>
2021-11-02 09:24:13 -0400

    Create Get route for news

---
+++ back-end/routes/getNews.js
@@ -0,0 +1,81 @@
+const axios = require("axios");
+const database = require("../data");
+var { userAssets, assetNews, allImages } = database;
+
+const coins = [];
+
+const getImagesAPI = async () => {
+  const images = "https://picsum.photos/v2/list?page=2&limit=20";
+  await axios
+    .get(images)
+    .then((res) => {
+      for (let i = 0; i < 20; ++i) {
+        allImages.push(res.data[i].download_url);
+      }
+    })
+    .catch((err) => {
+      if (err.response) {
+        console.log("Error response from API", err.response.status);
+      } else if (err.request) {
+        console.log("No response from API", err.response.status);
+      }
+    });
+};
+
+const getCryptoNews = async () => {
+  database.cryptoNews = [];
+  const url = "https://my.api.mockaroo.com/crypto.json?key=9371d6e0";
+  let isSucces = false;
+
+  await axios
+    .get(url)
+    .then((res) => {
+      database.cryptoNews = res.data;
+      isSucces = true;
+    })
+    .catch((err) => {
+      if (err.response) {
+        console.log("Error response from API", err.response.status);
+      } else if (err.request) {
+        console.log("No response from API", err.response.status);
+      }
+    });
+
+  return isSucces;
+};
+
+const getArticles = async () => {
+  for (let i = 0; i < userAssets.length; ++i) {
+    coins.push(userAssets[i].id);
+  }
+
+  // const date = new Date("2021-10-26");
+  const url = "https://my.api.mockaroo.com/articles.json?key=9371d6e0";
+  // const url = `https://newsapi.org/v2/everything?qInTitle=+${coins[i]}&from=${date}&language=en&sortBy=relevancy&apiKey=${process.env.REACT_APP_NEWS_API_KEY}&pageSize=20`
+
+  let isSucces = false;
+  await axios
+    .get(url)
+    .then((res) => {
+      let j = 0;
+      for (let i = 0; i < coins.length; ++i) {
+        assetNews[coins[i]] = res.data.slice(j, j + 20);
+        j = j + 20;
+      }
+      isSucces = true;
+    })
+    .catch((err) => {
+      if (err.response) {
+        console.log("Error response from API", err.response.status);
+      } else if (err.request) {
+        console.log("No response from API", err.response.status);
+      }
+    });
+  return isSucces;
+};
+
+var newsAPI = (module.exports = {
+  articlesAPI: getArticles,
+  imagesAPI: getImagesAPI,
+  cryptoAPI: getCryptoNews,
+});
--- back-end/routes/userData.js
+++ back-end/routes/userData.js
@@ -1,27 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const database = require("../data");
+const getNews = require("./getNews");
 
-//A constructor that create a user data object for all the data pertaining to a user
-class OwnedAsset {
-    constructor(id, quantityPurchased, unitPrice, datePurchased) {
-        this.id = id;
-        this.quantityPurchased = quantityPurchased;
-        this.unitPrice = unitPrice;
-        const [year, month, day] = datePurchased.split("/").reverse();
-        this.datePurchased = Date(year, month, day);
-    }
-}
-
-//An array that shall be used to store all the data pertaining to the assets of a user
-const DefaultUserAssets = [
-    new OwnedAsset("bitcoin", 2, 30000, "10/05/2021"),
-    new OwnedAsset("ethereum", 20, 2000, "12/06/2021"),
-    new OwnedAsset("polkadot", 2, 30, "13/08/2021"),
-];
+const { userAssets, assetNews, allImages } = database;
+const { articlesAPI, imagesAPI, cryptoAPI } = getNews;
 
 //Will return all the assets owned by the user
 router.get("/", (req, res) => {
-    res.status(200).json(DefaultUserAssets);
+  res.status(200).json(userAssets);
 });
 
 //Expects a JSON Object of the Form as Input
@@ -33,30 +20,65 @@ router.get("/", (req, res) => {
 // }
 // Will add the new data into the above array and return the newly added JSON object
 router.post("/", (req, res) => {
-    console.log(req.body);
-    const { id, quantityPurchased, unitPrice, stringDate } = req.body;
-    const datePurchased = new Date(stringDate);
-    const newUserAsset = {
-        id,
-        quantityPurchased,
-        unitPrice,
-        datePurchased,
+  console.log(req.body);
+  const { id, quantityPurchased, unitPrice, stringDate } = req.body;
+  const datePurchased = new Date(stringDate);
+  const newUserAsset = {
+    id,
+    quantityPurchased,
+    unitPrice,
+    datePurchased,
+  };
+  if (!id || !quantityPurchased || !unitPrice || !datePurchased) {
+    res.status(400).send({ ...newUserAsset, message: "Invalid Input." });
+  } else {
+    userAssets.push(newUserAsset);
+    res.status(200).json(newUserAsset);
+  }
+});
+
+//Get news from mockaroo (Mocking it for the time being)
+router.get("/cryptoNews", (req, res) => {
+  if (cryptoNews.length === 0) {
+    const articles = async () => {
+      const isSucces = await cryptoAPI();
+      if (isSucces === true)
+        res.status(200).json({ data: database.cryptoNews });
+      else res.status(500).send("Could not get data from API");
     };
-    if (!id || !quantityPurchased || !unitPrice || !datePurchased) {
-        res.status(400).send({ ...newUserAsset, message: "Invalid Input." });
-    } else {
-        DefaultUserAssets.push(newUserAsset);
-        res.status(200).json(newUserAsset);
-    }
+    articles();
+  } else {
+    res.status(200).send(cryptoNews);
+  }
+});
+
+router.get("/assetNews", (req, res) => {
+  if (Object.keys(assetNews).length === 0) {
+    const articles = async () => {
+      const isSucces = await articlesAPI();
+      if (isSucces === true) res.status(200).json(assetNews);
+      else res.status(500).send("Could not get data from API");
+    };
+    articles();
+  } else {
+    res.status(200).json(assetNews);
+  }
+});
+
+router.get("/images", (req, res) => {
+  if (allImages.length === 0) {
+    imagesAPI();
+  }
+  res.status(200).json(allImages);
 });
 
 // Will return the asset object assosciated with a particular asset id where asset id is the name of the coin, and an error otherwise
 router.get("/:id", (req, res) => {
-    const assetID = req.params.id;
-    console.log(assetID || "Nothing");
-    const getAsset = DefaultUserAssets.find((asset) => asset.id === assetID);
-    if (getAsset) res.status(200).json(getAsset);
-    else res.status(500).json({ message: "Asset with ID not found." });
+  const assetID = req.params.id;
+  console.log(assetID || "Nothing");
+  const getAsset = userAssets.find((asset) => asset.id === assetID);
+  if (getAsset) res.status(200).json(getAsset);
+  else res.status(500).json({ message: "Asset with ID not found." });
 });
 
 //Deletion to be implemented by Hanzallah
