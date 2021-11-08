c422739b4bc316bdb3e28e1d69066d9a7e598774
sashanksilwal <sashanksilwal@hotmail.com>
2021-11-07 23:05:47 -0500

    debugginh wordcloud

--- front-end/src/components/WordCloud/WordCloud.jsx
+++ front-end/src/components/WordCloud/WordCloud.jsx
@@ -1,19 +1,12 @@
-import React, { useState, useEffect, useRef, useMemo } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import Paper from "@mui/material/Paper";
 import "./WordCloud.css";
-import { coinLabelDataURL } from "../../back-end_routes";
 
 function WordCloud() {
-  const [text, setText] = useState([]);
-  const [coinData, setCoinData] = useState([]);
   const canvasA = useRef(null);
-  // const userID = "John";
-
-  const userCoins = useMemo(() => coinData.map((a) => a.id), [coinData]);
 
   useEffect(() => {
-    setText(userCoins);
     // getting the wordcloud
     const image = new Image();
     const canvas = canvasA.current.getContext("2d");
@@ -21,7 +14,7 @@ function WordCloud() {
     axios
       .get("/nfa/wordcloud")
       .then((res) => {
-        image.src = res.data;
+        image.src = res.data["data"];
       })
       .catch((err) => {
         return err;
