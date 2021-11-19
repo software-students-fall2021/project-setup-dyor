028c0108b87dfc91e2378609cbea02985a6e16cd
sashanksilwal <sashanksilwal@hotmail.com>
2021-11-19 17:16:07 -0500

    adding authentication backend code

---
+++ back-end/helpers/routeHelpers.js
@@ -0,0 +1,24 @@
+const Joi = require("joi");
+
+// helper to validate the inputs
+module.exports = {
+  validateBody: (schema) => {
+    return (req, res, next) => {
+      const result = schema.validate(req.body);
+      if (result.error) {
+        return res.status(400).json(result.error);
+      }
+      if (!req.value) {
+        req.value = {};
+      }
+      req.value["body"] = result.value;
+      next();
+    };
+  },
+  schemas: {
+    authSchema: Joi.object().keys({
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    }),
+  },
+};
---
+++ back-end/routes/users.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = require("express-promise-router")();
+const passport = require("passport");
+
+const passportConf = require("../passport");
+
+const { validateBody, schemas } = require("../helpers/routeHelpers");
+const UserController = require("../controllers/users");
+
+const passportSignIn = passport.authenticate("local", { session: false });
+router
+  .route("/signup")
+  .post(validateBody(schemas.authSchema), UserController.signUp);
+
+router
+  .route("/signin")
+  .post(
+    validateBody(schemas.authSchema),
+    passportSignIn,
+    UserController.signIn,
+  );
+
+module.exports = router;
