5ebc4461ee0c177bc50cdad4d6b60091cc775fba
Abdullah Javaid Chaudhry <aqhr050@gmail.com>
2021-11-28 19:53:15 -0500

    DO NOT MERGE

--- back-end/routes/coinPriceTimeSeries.js
+++ back-end/routes/coinPriceTimeSeries.js
@@ -23,70 +23,75 @@ router.get("/", (req, res) => {
 
   console.log("IN GET OF coinPriceTimeSeries ROUTE");
 
-  fs.readFile(
-    `./public/timeSeriesData/${coin_symbol}_${formatted_time_start}_${formatted_time_end}.json`,
-    "utf-8",
-    (err, jsonString) => {
-      //if the data has not been previously written to file it will now be fetched
-      if (err) {
-        console.log("FILE NOT PRESENT");
+  try {
+    fs.readFile(
+      `./public/timeSeriesData/${coin_symbol}_${formatted_time_start}_${formatted_time_end}.json`,
+      "utf-8",
+      (err, jsonString) => {
+        //if the data has not been previously written to file it will now be fetched
+        if (err) {
+          console.log("FILE NOT PRESENT");
 
-        console.log(
-          `DATA ${coin_symbol}_${formatted_time_start}_${formatted_time_end} NOT PRESENT`,
-        );
-        axios
-          .request(
-            `https://${baseURL}/v1/exchangerate/${coin_symbol}/USD/history`,
-            {
-              params: {
-                period_id,
-                time_start,
-                time_end,
-                limit,
-                apikey,
-              },
-            },
-          )
-          .then((response) => {
-            try {
-              const stringData = JSON.stringify(response.data);
-              fs.writeFile(
-                `./public/timeSeriesData/${coin_symbol}_${formatted_time_start}_${formatted_time_end}.json`,
-                stringData,
-                (err) => {
-                  if (err) console.log(err);
-                  else console.log("Succesful Writing.");
+          console.log(
+            `DATA ${coin_symbol}_${formatted_time_start}_${formatted_time_end} NOT PRESENT`,
+          );
+          axios
+            .request(
+              `https://${baseURL}/v1/exchangerate/${coin_symbol}/USD/history`,
+              {
+                params: {
+                  period_id,
+                  time_start,
+                  time_end,
+                  limit,
+                  apikey,
                 },
-              );
-              res.status(200).json(response.data);
-            } catch (err) {
-              console.log("STRINGIFY FAILED FOR NEW DATA.");
-            }
-          })
-          .catch((err) => {
-            res.status(500).json({
-              message: "FAILURE TO GET TIME SERIES DATA.",
+              },
+            )
+            .then((response) => {
+              try {
+                const stringData = JSON.stringify(response.data);
+                fs.writeFile(
+                  `./public/timeSeriesData/${coin_symbol}_${formatted_time_start}_${formatted_time_end}.json`,
+                  stringData,
+                  (err) => {
+                    if (err) console.log(err);
+                    else console.log("Succesful Writing.");
+                  },
+                );
+                res.status(200).json(response.data);
+              } catch (err) {
+                console.log("STRINGIFY FAILED FOR NEW DATA.");
+              }
+            })
+            .catch((err) => {
+              res.status(500).json({
+                message: "FAILURE TO GET TIME SERIES DATA.",
+              });
             });
-          });
-      } else {
-        console.log("FILE PRESENT");
-        //if the data has been previously written to file it shall be fetched from thereon
-        console.log(
-          `DATA ${coin_symbol}_${formatted_time_start}_${formatted_time_end} ALREADY PRESENT`,
-        );
-        // console.log(jsonString);
-        try {
-          const parsedJSON = JSON.parse(jsonString);
-          console.log("PARSE SUCCEEDED");
-          res.status(200).json(parsedJSON);
-        } catch (err) {
-          console.log(err);
-          console.log("PARSE FAILED FOR EXISTING DATA.");
-          res.sendStatus(500);
+        } else {
+          console.log("FILE PRESENT");
+          //if the data has been previously written to file it shall be fetched from thereon
+          console.log(
+            `DATA ${coin_symbol}_${formatted_time_start}_${formatted_time_end} ALREADY PRESENT`,
+          );
+          // console.log(jsonString);
+          try {
+            const parsedJSON = JSON.parse(jsonString);
+            console.log("PARSE SUCCEEDED");
+            res.status(200).json(parsedJSON);
+          } catch (err) {
+            console.log(err);
+            console.log("PARSE FAILED FOR EXISTING DATA.");
+            res.sendStatus(500);
+          }
         }
-      }
-    },
-  );
+      },
+    );
+  } catch (err) {
+    console.log("fs.readFile FAILED");
+    console.log(err);
+  }
 });
 
 module.exports = router;
